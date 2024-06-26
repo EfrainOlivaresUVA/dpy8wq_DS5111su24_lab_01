a744e2175c14610407cd45cbb1dc49b57a909428
EfrainOlivaresEv <eolivares@evidation.com>
2024-06-25 18:05:26 -0700

    passing tests two files

--- src/dpy8wq/__init__.py
+++ src/dpy8wq/__init__.py
@@ -1,1 +1,2 @@
 from dpy8wq.functions import add_one
+from dpy8wq.subtract import minus_one
--- tests/test_add_one.py
+++ tests/test_add_one.py
@@ -6,3 +6,6 @@ import dpy8wq as pkg
 
 def test_add_one():
     assert 5 == pkg.add_one(4)
+
+def test_minus_one():
+    assert 3 == pkg.minus_one(4)
